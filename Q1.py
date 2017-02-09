b90ae4af0da860061fa2685e2685a4c505331c42
JeetMakvana <jtmakvana@gmail.com>
2017-02-09 16:09:00 +0530

    Q1

---
+++ Q1.py
@@ -0,0 +1,12 @@
+
+a=raw_input("Enter the length of Rectangle")
+b=raw_input("Enter the width of Rectangle")
+c=int(a)
+d=int(b)
+peri=2*(c+d)
+print "Perimeter of Rectangle is"
+print peri
+j=int(a)*int(b)
+print "Area of Rectangle is"
+print(j)
+
