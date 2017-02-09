c6131ef7fdf77163e8e42d7bd5d7c3843684d8c7
JeetMakvana <jtmakvana@gmail.com>
2017-02-09 16:14:07 +0530

    Q2

---
+++ Q2.py
@@ -0,0 +1,11 @@
+# Python Program to find Area Of Circle using Radius
+ 
+PI = 3.14
+radius = float(input(' Enter the radius of a circle: '))
+area = PI * radius * radius
+circumference = 2 * PI * radius
+ 
+print(" Area Of a Circle")
+print(area)
+print(" Circumference Of a Circle")
+print(circumference)
